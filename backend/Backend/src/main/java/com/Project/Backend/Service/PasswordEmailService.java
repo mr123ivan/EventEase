f580144f17522c0c192dd708e4270d854ce6f130
Miklos Bolarde <bolarde321@gmail.com>
2025-09-22 16:37:17 +0800

    added email checker to subcontractor creator and remove any hardcodded email password put it in env

--- backend/Backend/src/main/java/com/Project/Backend/Service/PasswordEmailService.java
+++ backend/Backend/src/main/java/com/Project/Backend/Service/PasswordEmailService.java
@@ -1,6 +1,7 @@
 package com.Project.Backend.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,13 @@ public class PasswordEmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.from}")
+    private String fromEmail;
+
     public void sendPasswordEmail(String to, String password) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("aeronespina0@gmail.com");
+            message.setFrom(fromEmail);
             message.setTo(to);
             message.setSubject("Your EventEase Subcontractor Account Password");
             message.setText("Welcome to EventEase!\n\n" +
